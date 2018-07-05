febcfa18d739f1cd229a43638f883858e71f8d65
Rasna Bura Gohain <rasna.maarten@gmail.com>
2018-07-05 12:51:28 +0200

    minor cleaning

--- src/store/connection.js
+++ src/store/connection.js
@@ -31,6 +31,7 @@ export const reducer = (state = initialState, action) => {
           name: action.payload.name,
         }
       }
+
       return state
 
     case close:
--- src/store/users.js
+++ src/store/users.js
@@ -35,7 +35,6 @@ export const reducer = (state = initialState, action) => {
         }
       }
 
-
       return state
 
     case close:
